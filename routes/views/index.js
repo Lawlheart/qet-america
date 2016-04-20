e1e89fa3bb6acb5a71c05c92330ff4b75d302e1c
Juan Martínez <jbmartinezs@yahoo.com>
2016-04-20 03:28:56 +0000

    add static translation
    It uses i18next and passes the t() function to the templates where
    can be used as t('some string to translate'). If a string is not
    found in the translation file, it is added to ns.missing.json,
    where ns is the namespace.
    
    We can use namespaces to keep the strings organized, for example,
    the "navbar" namespace can contain all the strings related to the
    navigation bar.
    
    The current translation files are only for demonstration purposes.

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,15 +1,39 @@
 var keystone = require('keystone');
 
+var i18next = require('i18next');
+var Backend = require('i18next-node-fs-backend');
+var path = require('path');
+
+
 exports = module.exports = function(req, res) {
-	
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
-	
-	// locals.section is used to set the currently selected
-	// item in the header navigation.
-	locals.section = 'home';
-	
-	// Render the view
-	view.render('index');
-	
+
+	i18next
+	.use(Backend)
+	.init({
+		lng: req.language,
+		fallbackLng: 'en',
+		ns: 'index',
+		defaultNS: 'index',
+		saveMissing: true,
+		backend: {
+			// path where resources get loaded from
+			loadPath: path.resolve('./locales') + '/{{lng}}/{{ns}}.json',
+			// loadPath: '/locales/{{lng}}/{{ns}}.json',
+
+			// path to post missing resources
+			addPath: path.resolve('./locales') + '/{{lng}}/{{ns}}.missing.json',
+
+			// jsonIndent to use when storing json files
+			jsonIndent: 2
+		}
+	}, function(err, t) {
+		locals.t = t;
+		// locals.section is used to set the currently selected
+		// item in the header navigation.
+		locals.section = 'home';
+
+		view.render('index');
+	});
 };
