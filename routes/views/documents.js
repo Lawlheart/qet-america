9845f416245a7a4e3f7f641347358206640cda78
Juan Martínez <jbmartinezs@yahoo.com>
2016-07-14 20:29:54 +0000

    Fix a bug with translations for Documents section

--- routes/views/documents.js
+++ routes/views/documents.js
@@ -1,14 +1,19 @@
 var keystone = require('keystone');
 
+var i18next = require('i18next');
+
 exports = module.exports = function(req, res) {
-  
+
   var view = new keystone.View(req, res);
   var locals = res.locals;
-  
+
   // Set locals
   locals.section = 'documents';
 	
 	view.query('documents', keystone.list('Document').model.find());
   
-  view.render('documents');
+  i18next.setDefaultNamespace('index');
+	i18next.loadNamespaces('index', function(err, t) {
+		view.render('documents');
+	});
 };
